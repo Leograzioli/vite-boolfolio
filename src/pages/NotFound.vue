0047c9fd34af86bf65c3a5573747826576c5e2af
Leograzioli <leograzioli@gmail.com>
2023-01-20 16:45:13 +0100

    add not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<template>
+    <main>
+        <div class="d-flex h-100 justify-content-center mt-5">
+            <h2>404 NOT FOUND</h2>
+        </div>
+    </main>
+</template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from './pages/AppHome.vue';
 import AboutUs from './pages/AboutUs.vue';
 import ProjectsList from './pages/ProjectsList.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -20,6 +21,11 @@ const router = createRouter({
             path: '/projects',
             name: 'projects',
             component: ProjectsList
+        },
+        {
+            path: "/:pathMatch(.*)*",
+            name: "not-found",
+            component: NotFound,
         },
     ]
 });
