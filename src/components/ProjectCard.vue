68d90252752c73165512d30c6eac8a25fd0472dd
Leograzioli <leograzioli@gmail.com>
2023-01-23 14:41:52 +0100

    add max lenght to project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -4,6 +4,19 @@ export default {
     props: {
         proj: Object
     },
+    data () {
+        return {
+            maxLength: 100
+        }
+    },
+    computed: {
+        descriptionLenght () {
+            if (this.proj.description && this.proj.description.length > this.maxLength) {
+                return this.proj.description.substring(0, this.maxLength) + '...';
+            }
+            return this.proj.description
+        }
+    }
 }
 </script>
 
@@ -15,7 +28,7 @@ export default {
                 <h6 v-if="proj.type" class="card-subtitle mb-2 text-primary text-center">{{ proj.type.name }}</h6>
                 <h6 v-else class="card-subtitle mb-2 text-primary text-center">No Type</h6>
                 <span v-for="tech in proj.technologies" :key="tech.id" class="card-subtitles text-muted"> #{{ tech.name }}</span>
-                <p class="card-text">{{ proj.description }}</p>
+                <p class="card-text">{{ descriptionLenght }}</p>
                 <a href="#" class="card-link btn btn-primary">See Project</a>
             </div>
         </div>
